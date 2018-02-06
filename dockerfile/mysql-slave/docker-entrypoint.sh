33a401fc41b9d45ee6c7685bd335cad75dea0f66
TroyYing <32924116+TroyYing@users.noreply.github.com>
2018-02-06 18:34:37 +0800

    Update docker-entrypoint.sh

--- dockerfile/mysql-slave/docker-entrypoint.sh
+++ dockerfile/mysql-slave/docker-entrypoint.sh
@@ -174,7 +174,7 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 		fi
 
                 echo "STOP SLAVE;" | "${mysql[@]}"
-                echo "CHANGE MASTER TO master_host='$MYSQL_MASTER_SERVICE_HOST', master_user='$MYSQL_REPLICATION_USER', master_password='$MYSQL_REPLICATION_PASSWORD' ;" | "${mysql[@]}"
+                echo "CHANGE MASTER TO master_host='$MYSQL_MASTER_HOST', master_user='$MYSQL_REPLICATION_USER', master_password='$MYSQL_REPLICATION_PASSWORD' ;" | "${mysql[@]}"
                 echo "START SLAVE;" | "${mysql[@]}"
 
 
